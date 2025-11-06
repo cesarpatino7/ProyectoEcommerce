96a54c6046878329ad072c076d50e1af3ef5e1b3
Cesar Patino <cesardavidpatinovera@gmail.com>
2025-11-05 21:55:22 -0300

    se agrega v1 al endpoint

--- backend/src/main/java/com/taller/ingenieria/api/controller/ResenasProductoControllerImpl.java
+++ backend/src/main/java/com/taller/ingenieria/api/controller/ResenasProductoControllerImpl.java
@@ -12,7 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/resenas")
+@RequestMapping("/api/v1/resenas")
 public class ResenasProductoControllerImpl implements ResenasProductoController {
 
     @Autowired
